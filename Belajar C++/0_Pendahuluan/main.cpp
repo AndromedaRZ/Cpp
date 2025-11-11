4cbbe7d7c64410bf8cbd6173b50b9ba2e3ff57b7
AndromedaRZ <rizkyricky859@gmail.com>
2025-11-11 13:04:37 +0700

    first commit

---
+++ Belajar C++/0_Pendahuluan/main.cpp
@@ -0,0 +1,36 @@
+// PENDAHULUAN
+//
+// Perkenalan singkat C++
+//
+// C dan C++ itu adalah dua bahasa pemrograman yang berbeda
+// dinamakan C++ karena pada saat itu pendiri bahasa C yang bernama Bjarne Stroustrup
+// ingin membuat bahasa pemrograman C yang lebih kompleks dengan menambahkan fitur-fitur yang lebih kompleks
+// saking banyaknya fitur yang ditambahkan maka namanya pun berubah menjadi C++ (++ merujuk pada fitur yang bertambah banyak)
+//
+//
+// Standarisasi C++ dari tahun ke tahun
+// 1985 --> versi awal
+// 1998 --> versi major/major update
+// 2003 --> revisi dari versi 2003
+// 2011 --> versi major/major update
+// 2014 --> revisi dari versi 2011
+// 2017 --> revisi dari versi 2011
+// 2020 --> versi major/major update
+// 2023 --> versi major/major update
+// 
+// Program C++ bisa masuk ke setiap platform seperti windows, linux, mac, android, bahkan xbox
+// hal ini dikarenakan yang namanya compiler, C++ akan masuk ke compiler yang nantinya ditranslate
+// agar bisa jalan pada platform-platform tersebut, setiap platform memiliki yang namanya Machine Code.
+// keuntungan dari Machine Code tersebut adalah saat programnya berjalan pada platform tersebut, platform tersebut
+// akan melihat code C++ nya dari Machine Code tersebut.
+//
+//
+// CARA KERJA BAHASA C++
+// Dimulai saat pertama kali kita mengetik sebuah code di file C++ kita, karena isinya berbahasa inggris
+// jadi komputer tidak akan langsung paham, oleh karena itu C++ bisa melakukan yang namanya Compile, yaitu
+// merubah bahasa kode yang kita ketikan atau disebut source code tadi menjadi sebuah instruksi mesin yang dipahami komputer atau bahasa komputer
+//
+//
+// Sebelum kita menjalankan file C++, kita harus mengcompile-nya terlebih dahulu, 
+// jika menggunakan sistem operasi windows, kita bisa menggunakan GCC atau G++ pada terminal
+// 
---
+++ Belajar C++/4_Deklarasi_dan_Cin/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std; // jika menggunakan 'using namespace std' ini, maka saat menggunakan standard library-nya kita tidak perlu lagi mengetikan 'std::'
+
+int main() {
+    int a; // deklarasi sebuah variabel
+    a = 10; // memasukan nilai/value ke dalam sebuah variabel
+    cout << a << endl;
+
+    int b;
+    cout << "Masukan nilai: ";
+    cin >> b; // memasukan nilai/value ke dalam sebuah variabel dengan menggunakan input user
+    cout << "Nilai yang anda masukan adalah: ";
+    cout << b << endl;
+
+    cin.get();
+    return 0;
+}
